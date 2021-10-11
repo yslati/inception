1587411395dcf2df8d1ff8bc6084878d3f08ec63
yslati <yslati@student.1337.ma>
2021-10-11 18:33:03 +0100

    save

---
+++ srcs/MariaDB/tools/script.sh
@@ -0,0 +1,14 @@
+if [ ! -f "/var/lib/mysql/ib_buffer_pool" ];
+then
+        /etc/init.d/mariadb setup &>/dev/null
+        service mariadb start &>/dev/null
+        echo "CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';" | mysql -u root
+        echo "CREATE DATABASE wordpress;" | mysql -u root
+        echo "GRANT ALL PRIVILEGES on *.* to 'admin'@'%' IDENTIFIED BY 'admin';" | mysql -u root
+        echo "FLUSH PRIVILEGES;" | mysql -u root
+
+fi
+sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
+service mariadb restart
+
+/usr/bin/mysqld --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mariadb/plugin --user=mysql --pid-file=/run/mysqld/mariadb.pid
